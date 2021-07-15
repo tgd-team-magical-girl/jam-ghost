c26cb76d47e20f492b41e550299bf599462e1202
Nikki Liu <55368578+LastLightStudios@users.noreply.github.com>
2021-07-15 00:05:16 -0400

    Movement (#3)
    * Movable character added
    * Adjusted per comments

--- magicalgirlbossfight/src/magicalgirlbossfight/app.py
+++ magicalgirlbossfight/src/magicalgirlbossfight/app.py
@@ -2,15 +2,13 @@
 A game jam game of Magical Girls and big boss fights.
 """
 import ppb
-
+from .players import Player
 
 class magicalgirlbossfight(ppb.BaseScene):
     def __init__(self, **props):
         super().__init__(**props)
 
-        self.add(ppb.Sprite(
-            image=ppb.Image('magicalgirlbossfight/resources/magicalgirlbossfight.png'),
-        ))
+        self.add(Player())
 
 
 def main():
---
+++ magicalgirlbossfight/src/magicalgirlbossfight/players.py
@@ -0,0 +1,32 @@
+import ppb
+from ppb import keycodes
+from ppb.events import KeyPressed, KeyReleased, Update
+
+
+class Player(ppb.Sprite):
+    image = ppb.Image('magicalgirlbossfight/resources/magicalgirlbossfight.png')
+    position = ppb.Vector(0, 0)
+    move_vector = ppb.Vector(0, 0)
+
+    def on_update(self, event: ppb.events.Update, signal):
+        self.position += self.move_vector
+
+    def on_key_pressed(self, event: ppb.events.KeyPressed, signal):
+        if event.key is keycodes.W:
+            self.move_vector += ppb.directions.Up
+        elif event.key is keycodes.A:
+            self.move_vector += ppb.directions.Left
+        elif event.key is keycodes.S:
+            self.move_vector += ppb.directions.Down
+        elif event.key is keycodes.D:
+            self.move_vector += ppb.directions.Right
+
+    def on_key_released(self, event: ppb.events.KeyReleased, signal):
+        if event.key is keycodes.W:
+            self.move_vector -= ppb.directions.Up
+        elif event.key is keycodes.A:
+            self.move_vector -= ppb.directions.Left
+        elif event.key is keycodes.S:
+            self.move_vector -= ppb.directions.Down
+        elif event.key is keycodes.D:
+            self.move_vector -= ppb.directions.Right
